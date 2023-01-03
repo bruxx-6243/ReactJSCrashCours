2e9c332b2d331905384c889d571b142915aca2c5
Fariol Blondeau <blondeau.nbif@mail.ru>
2023-01-03 18:02:07 +0300

    React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from "react";
-import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const [predicted, setPredicted] = useState(null);
-
-  const handleAge = async () => {
-    try {
-      const response = await axios.get(`https://api.agify.io/?name=${name}`);
-      setPredicted(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <div className="App">
-      <h1>Name: {predicted?.name}</h1>
-      <h1>Predicted age: {predicted?.age}</h1>
-      <h1>Predicted count: {predicted?.count}</h1>
-      <input
-        type="text"
-        placeholder="Enter the name"
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
-      <button onClick={handleAge}>Get The Age</button>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>404 Page not found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
